389294ebc95be97679b96db3c8346dc91fbe3ef3
Allan N López <anlopez_1294@hotmail.com>
2025-08-13 20:25:26 -0600

    calculadora

---
+++ dev-utilities/dev-utilities/Controllers/TimeFormatterController.cs
@@ -0,0 +1,33 @@
+﻿using dev_utilities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dev_utilities.Controllers
+{
+    public class TimeFormatterController : Controller
+    {
+        TimeFormatter model = new TimeFormatter();
+        public IActionResult Index()
+        {
+            model.TimeFormat = "00:00:00";
+
+            return View(model);
+        }
+
+        public IActionResult FormatFromMinutes(TimeFormatter model)
+        {            
+            var mins = model.Minutes > 0 ? model.Minutes : 0;           
+            
+
+            return View(model);
+        }
+
+        public IActionResult FormatFromSeconds(TimeFormatter model)
+        {
+            int secs = model.Seconds > 0 ? model.Seconds : 0;
+
+            model.TimeFormat = "00:00:"+secs.ToString();
+
+            return View(model);
+        }
+    }
+}
---
+++ dev-utilities/dev-utilities/Models/TimeFormatter.cs
@@ -0,0 +1,16 @@
+﻿namespace dev_utilities.Models
+{
+    public class TimeFormatter
+    {
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public string TimeFormat { get; set; }
+
+        public TimeFormatter() {
+            this.Seconds = 0;
+            this.Minutes = 0;
+            this.TimeFormat = "";
+        }
+    }
+}
